82deaeeabe8dea180e567d1b338771418655318b
Dominguez Gaston Matias <gaston.dominguez@accenture.com>
2022-06-24 17:10:47 -0300

    add heartstone api services

---
+++ app/services/cards.ts
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+// const options = {
+//   method: 'GET',
+//   url: process.env.API_HEARTH_STONE,
+//   headers: {
+//     'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+//     'X-RapidAPI-Host': 'omgvamp-hearthstone-v1.p.rapidapi.com'
+//   }
+// };
+
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
+
+export async function hearthStoneInfo(text: string) {
+  await axios({
+    method: 'GET',
+    url: text,
+    baseURL: process.env.API_HEARTH_STONE,
+    headers: {
+      'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+      'X-RapidAPI-Host': 'omgvamp-hearthstone-v1.p.rapidapi.com'
+    }
+  })
+}
+
+export default {
+  hearthStoneInfo
+};
