ef3685bdbcb70ff405fe5c90fdae0eb99251aadf
Dominguez Gaston Matias <gaston.dominguez@accenture.com>
2022-06-27 11:20:13 -0300

    add hearthStone Api

--- app/services/cards.ts
+++ app/services/cards.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const urlApi = `${process.env.API_HEARTH_STONE}?format=json`;
 
-export const getInfo = async () => 
+export const getInfo = async (): Promise<any> => 
   await axios
     .get(urlApi)
     .then((response: any) => response)
