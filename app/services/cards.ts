998acd29d5367810ed9d6af66c5bd357ff302d1f
Dominguez Gaston Matias <gaston.dominguez@accenture.com>
2022-06-27 15:33:06 -0300

    update libraries

--- app/services/cards.ts
+++ app/services/cards.ts
@@ -25,10 +25,34 @@ export interface Patch {
   type: string;
 }
 
+// export interface Options {
+//   method:  string;
+//   url:     UrlApi;
+//   headers: Headers;
+// }
+
+// export interface Headers {
+//   "X-RapidAPI-Key":  string;
+//   "X-RapidAPI-Host": string;
+// }
+
+// export interface UrlApi {
+//   type: string;
+// }
+
 const urlApi = `${process.env.API_HEARTH_STONE}?format=json`;
 
+const options = {
+  method: 'GET',
+  url: urlApi,
+  headers: {
+    'X-RapidAPI-Key': process.env.API_HEARTH_STONE_KEY,
+    'X-RapidAPI-Host': process.env.API_HEARTH_STONE_HOST
+  }
+} as const;
+
 export const getInfo = async (): Promise<Info> => {
-  const response = await axios.get(urlApi);
+  const response = await axios.request(options);
   return response.data;
 };
 
