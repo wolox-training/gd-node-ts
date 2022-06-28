8082373974c2759cd11f07879c430e78730f9696
Dominguez Gaston Matias <gaston.dominguez@accenture.com>
2022-06-28 11:12:29 -0300

    add routes endpoint for hearthstone external api

--- app/routes.ts
+++ app/routes.ts
@@ -3,6 +3,7 @@ import { Application } from 'express';
 import { healthCheck } from './controllers/healthCheck';
 import { getUsers, getUserById, createUser } from './controllers/users';
 import { getTodos } from './controllers/todos';
+import { getHSinfo, getHScards } from './controllers/hearthstone'
 
 export const init = (app: Application): void => {
   app.get('/health', healthCheck);
@@ -10,4 +11,6 @@ export const init = (app: Application): void => {
   app.post('/users', createUser);
   app.get('/users/:id', getUserById);
   app.get('/todos', getTodos);
+  app.get('/info', getHSinfo);
+  app.get('/cards', getHScards);
 };
