66e5b1ee2428c5c3ea1d8343c6b01a204e4dff83
Dominguez Gaston Matias <gaston.dominguez@accenture.com>
2022-06-29 11:29:06 -0300

    update controller and services for external api

--- app/controllers/hearthstone.ts
+++ app/controllers/hearthstone.ts
@@ -2,14 +2,16 @@ import { Response, Request, NextFunction } from 'express';
 import { Info, Allcards } from '../constants';
 import { getInfo, getAllCard } from '../services/cards';
 
-export function getHSinfo(_: Request, res: Response, next: NextFunction): Promise<Response | void> {
-  return getInfo()
+export function getHSinfo(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
+  const { path, method } = req;
+  return getInfo(path, method)
     .then((info: Info) => res.send(info))
     .catch(next);
 }
 
-export function getHScards(_: Request, res: Response, next: NextFunction): Promise<Response | void> {
-  return getAllCard()
+export function getHScards(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
+  const { path, method } = req;
+  return getAllCard(path, method)
     .then((cards: Allcards) => res.send(cards))
     .catch(next);
 }
