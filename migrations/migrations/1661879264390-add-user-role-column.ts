ac700dabdb12b7ae7b650cee2a7ce51cc5fc7caa
Dominguez Gaston Matias <gaston.dominguez@accenture.com>
2022-08-31 10:38:49 -0300

    update migration role

--- migrations/migrations/1661879264390-add-user-role-column.ts
+++ migrations/migrations/1661879264390-add-user-role-column.ts
@@ -1,11 +1,6 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AddUserRoleColumn1661879264390 implements MigrationInterface {
-  // public async up(queryRunner: QueryRunner): Promise<any> {
-  // }
-
-  // public async down(queryRunner: QueryRunner): Promise<any> {
-  // }
 
   async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
