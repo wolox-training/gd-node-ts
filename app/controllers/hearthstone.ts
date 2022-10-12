931c2f118872b1308596d55bfaacaa783c461d2b
Dominguez Gaston Matias <gaston.dominguez@accenture.com>
2022-10-12 13:51:41 -0300

    update buy-card

--- app/controllers/hearthstone.ts
+++ app/controllers/hearthstone.ts
@@ -74,5 +74,5 @@ export default {
   getHSinfo,
   getHScards,
   createHScard,
-  createHSset,
+  createHSset
 };
--- app/services/cards.ts
+++ app/services/cards.ts
@@ -60,5 +60,5 @@ export default {
   getInfo,
   getAllCard,
   createCard,
-  getSet,
+  getSet
 };
